c736fdaf20a18c056d21e3eae75ab79789507b9c
Liana <74722404+liaaana@users.noreply.github.com>
2023-09-12 15:15:22 +0000

    Week01 done

---
+++ week01/ex2.sh
@@ -0,0 +1,4 @@
+nano hello.txt
+cat hello.txt
+rm hello.txt
+history > ~/week01/ex2.txt
---
+++ week01/ex3.sh
@@ -0,0 +1,23 @@
+date 
+sleep 3
+mkdir ~/week01/root
+date 
+sleep 3
+touch ~/week01/root/root.txt
+ls -tr / >  ~/week01/root/root.txt
+
+date 
+sleep 3
+mkdir ~/week01/home
+date 
+sleep 3
+touch ~/week01/home/home.txt
+ls -tr ~ > ~/week01/home/home.txt
+date 
+sleep 3
+
+cat ~/week01/root/root.txt
+cat ~/week01/home/home.txt
+
+ls ~/week01/root/
+ls ~/week01/home/
